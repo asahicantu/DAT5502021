45bbabff1d14a34a0ffbfa5f0d2c9903dd144d0e
Asahi Cantu <asahicantu@outlook.com>
2021-04-11 12:46:20 +0200

    Added max_freq parameter

--- Utils/Feature.py
+++ Utils/Feature.py
@@ -18,11 +18,11 @@ All returned spectrogram have a normalized wave amplitude of 0-1 decibels
 #%%
 class Feature:
     DB_RANGE = 80.0
-    def __init__(self,midi_time_step, midi_notes, file_path, sample_rate= 16000, verbose=False):
+    def __init__(self,midi_time_step, midi_notes, file_path, sample_rate= 16000,max_freq=20000, verbose=False):
         try:
             self.VERBOSE = verbose
             self.Sample_rate = sample_rate
-            data = Feature._get_wav_features(file_path, sample_rate=self.Sample_rate, verbose=self.VERBOSE)
+            data = Feature._get_wav_features(file_path, sample_rate=self.Sample_rate, max_freq=max_freq, verbose=self.VERBOSE)
             self.Name = os.path.split(os.path.splitext(file_path)[0])[-1]
             self.Midi_Notes = midi_notes
             self.Midi_Time_Step = midi_time_step
@@ -61,12 +61,11 @@ class Feature:
         return midi_2_audio_time
     
     @staticmethod
-    def _get_wav_features(filepath, sample_rate, verbose=False):
+    def _get_wav_features(filepath, sample_rate,max_freq, verbose=False):
         try:
             sample_rate = sample_rate
             n_mels=40 #128 
             min_freq=27.5 
-            max_freq=20000
             n_mfcc=128  # Original was 128
             n_bins=88
 
