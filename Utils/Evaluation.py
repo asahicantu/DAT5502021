d3707cd47c80894bc435bd1c815f2a30b4c5b6be
Tim Bauerle <tim.bauerle@rwth-aachen.de>
2021-04-12 12:01:52 +0200

    Template for training and best model selection

--- Utils/Evaluation.py
+++ Utils/Evaluation.py
@@ -16,3 +16,21 @@ Returns the confusion matrix of key pressing being predicted correctly
 def get_confusion_matrix(predictions, groud_truth):
     return tf.math.confusion_matrix(predict_keys(tf.reshape(predictions,[-1]), 0.5), tf.reshape(groud_truth, [-1]))
 
+
+def get_metric(predictions, groud_truth, metric='accuracy'):
+    predictions = predict_keys(tf.reshape(predictions,[-1]))
+    labels = tf.reshape(groud_truth, [-1])
+    if metric=='accuracy':
+        ac = tf.keras.metrics.Accuracy()
+        ac.update_state(predictions, labels)
+        return ac.result().numpy()
+    elif metric=='precision':
+        pr = tf.keras.metrics.Precision()
+        pr.update_state(predictions, labels)
+        return pr.result().numpy()
+    elif metric=='recall':
+        re = tf.keras.metrics.Recall()
+        re.update_state(predictions, labels)
+        return re.result().numpy()
+    else:
+        print("unknown metric")
