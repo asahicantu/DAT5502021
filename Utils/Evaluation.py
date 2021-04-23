44ee3d61b802cbf458152d4d2012eeba25fc3aff
Asahi Cantu <asahicantu@outlook.com>
2021-04-23 19:24:51 +0200

    Added graphs and new D2 models

--- Utils/Evaluation.py
+++ Utils/Evaluation.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import itertools
 from scipy.stats import ttest_rel
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import auc
@@ -202,3 +203,35 @@ def group_labels_LSTM_M2M(label_dict):
     return out
 
 
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None):
+    
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    tick_marks = np.arange(len(target_names))
+    plt.xticks(tick_marks, target_names, rotation=45)
+    plt.yticks(tick_marks, target_names)
+
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    thresh = cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        text = "{:,}".format(cm[i, j]) + '\n' + "{:.2%}".format(cm_normalized[i, j])
+        plt.text(j, i,text , horizontalalignment="center",color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    return plt
